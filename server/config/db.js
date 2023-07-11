851743acaea21732998e916f4260b5d38b0b5b6f
Prathamesh Wadile <3unnycr@gmail.com>
2023-07-11 16:52:17 +0530

    Some fixing related to renaming and dotenv connections

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,8 @@
 const moongoose = require("moongoose");
 
-const connectDB = async () => {
-  await moongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const connectDB = async (url) => {
+  await moongoose.connect(url);
+
   console.log("Database Connected.");
 };
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 // ================= Imports ===================== //
+require('dotenv').config();
 const express = require("express");
 const connectDB = require("./config/db");
-const errorHandler = require("./middleware/errorHandler");
+const errorHandler = require("./middleware/errorHandlerMiddleware");
 const cors = require('cors');
 const http = require("http");
 // ================= Imports ===================== //
@@ -19,11 +20,17 @@ const app = express();
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(errorHandler);
+// app.use('/api/auth', require('./routes/auth'));
+// app.use('/api/user', require('./routes/user'));
+// app.use('/api/superintendent', require('./routes/superintendent'));
 // ================= Express Setting for server ===================== //
 
 // ================= Server Connection ===================== //
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.SERVER_PORT || 3001;
 
 const server = http.createServer(app);
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // ================= Server Connection ===================== //
+// ================= Database Connection ===================== //
+connectDB(process.env.MONGO_URI);
+// ================= Database Connection ===================== //
